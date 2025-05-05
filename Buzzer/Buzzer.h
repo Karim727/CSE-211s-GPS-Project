85838a2375f86fc7c65808926213c9079ef09557
unknown <shihab.rehan@gmail.com>
2025-05-05 18:20:17 +0300

    bit_Utilities: (delay_ms.h & delay_ms.c) Buzzer: buzzer.h & buzzer.c

---
+++ Buzzer/Buzzer.h
@@ -0,0 +1,20 @@
+#ifndef BUZZER_H
+#define BUZZER_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ void activate_Buzzer(void);
+
+ void deActivate_Buzzer(void);
+
+ void callBuzzer(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
